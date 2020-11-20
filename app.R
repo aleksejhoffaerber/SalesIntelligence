f277322737f95d2d5e9f661ed3a269a642fa17a7
KaroRonty <karo.ronty@gmail.com>
2020-11-20 20:12:30 +0100

    Efficiency improvements and small fixes

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ library(tidyverse)
 library(data.table)
 library(strucchange)
 
-rm(list = ls())
+p_value_threshold <- 0.05
 
 # Data reading and cleaning -----------------------------------------------
 
@@ -30,53 +30,25 @@ data_monthly <- data_by_invoice %>%
          Price > 0,
          # Remove those without numbers in product ID
          !str_detect(product, "^[[:alpha:]]")) %>% 
-  group_by(yearmonth, product) %>%
+  group_by(yearmonth, product) %>% 
   # Calculate amount of sales, average prices, revenue and receipt amount
   summarise(quantity_sum = sum(Quantity, na.rm = TRUE),
-            price_mean = weighted.mean(Price,Quantity, na.rm = TRUE),
+            price_mean = weighted.mean(Price, Quantity, na.rm = TRUE),
             revenue = sum(Price * Quantity),
             n_receipts = n()) %>% 
   group_by(product) %>% 
   # Keep only products with two years of observations or more
   mutate(n_months = n()) %>% 
-  ungroup() %>%
-  filter(n_months >= 24) %>%
-  arrange(product,yearmonth)
+  ungroup() %>% 
+  filter(n_months >= 24) %>% 
+  arrange(product, yearmonth)
 
-#### Test for Structural Change ####
-# Remove series with significant brakes. 
-
-# The functions performs a Cumulative sum test for parameter stability
-# and returns the p - vale
-
-structual_test <- function(prod) {
-  
-  series <- data_monthly %>% 
-    filter(product==prod) %>%
-    select(price_mean)
-  
-  price.cum <- efp(series$price_mean ~ 1,
-                   type = "Rec-CUSUM")
-  test <- sctest(price.cum)
-  
-  p <- cbind(prod,test$p) %>%
-    as.data.frame()
-  
-  return(p)
-}
-
-# Loop though all products
-p_values <- tibble()
-for (id in unique(data_monthly$product)) {
-  structual_test(id) %>%
-    rbind(p_values,.) -> p_values
-}
-
-# Remove all products with significant structural brakes
-p_values %>%
-  rename(product = prod,
-         p_value = V2) %>%
-  filter(is.na(p_value)==F) %>%
-  filter(p_value > 0.05) %>%
-  select(product) %>%
-  merge(.,data_monthly) -> data_monthly
+# Remove products with significant breaks
+data_to_arima <- data_monthly %>% 
+  group_by(product) %>% 
+  do(efp = efp(.$price_mean ~ 1,
+               type = "Rec-CUSUM")) %>% 
+  mutate(p_value = sctest(efp)$p) %>% 
+  filter(p_value > p_value_threshold) %>%
+  select(product) %>% 
+  inner_join(data_monthly)
